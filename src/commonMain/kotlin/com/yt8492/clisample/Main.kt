b65145534425885fedccd3959de8a2a90430f195
yt8492 <tmymyut2000@gmail.com>
2020-08-08 09:56:49 +0900

    fix code style

--- src/commonMain/kotlin/com/yt8492/clisample/Main.kt
+++ src/commonMain/kotlin/com/yt8492/clisample/Main.kt
@@ -7,10 +7,25 @@ import kotlinx.cli.required
 
 fun main(args: Array<String>) {
     val parser = ArgParser("cli_sample")
-    val argument by parser.argument(type = ArgType.String, description = "argument description")
-    val option1 by parser.option(type = ArgType.Int, shortName = "op1", description = "option 1")
-    val option2 by parser.option(type = ArgType.Boolean, shortName = "op2", description = "option 2").default(false)
-    val option3 by parser.option(type = ArgType.Choice(listOf("foo", "bar", "baz")),  shortName = "op3", description = "option 3").required()
+    val argument by parser.argument(
+        type = ArgType.String,
+        description = "argument description"
+    )
+    val option1 by parser.option(
+        type = ArgType.Int,
+        shortName = "op1",
+        description = "option 1"
+    )
+    val option2 by parser.option(
+        type = ArgType.Boolean,
+        shortName = "op2",
+        description = "option 2"
+    ).default(false)
+    val option3 by parser.option(
+        type = ArgType.Choice(listOf("foo", "bar", "baz")),
+        shortName = "op3",
+        description = "option 3"
+    ).required()
     parser.parse(args)
     println("argument: $argument")
     println("option1: $option1")
